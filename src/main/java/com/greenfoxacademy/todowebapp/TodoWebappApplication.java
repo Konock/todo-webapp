47b6e6c0305faae7d0c5a67ce03a0dec71cf6471
Konock <bkonock@gmail.com>
2018-09-20 16:37:57 +0200

    initialize webapp

---
+++ src/main/java/com/greenfoxacademy/todowebapp/TodoWebappApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.todowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWebappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodoWebappApplication.class, args);
+  }
+}
