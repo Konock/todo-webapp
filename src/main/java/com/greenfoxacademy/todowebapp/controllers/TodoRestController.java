e6499b05c896af67119c82d657ddddb404e31c96
Konock <bkonock@gmail.com>
2018-10-11 14:17:48 +0200

    switched mysql connection data with environment variables

--- src/main/java/com/greenfoxacademy/todowebapp/controllers/TodoRestController.java
+++ src/main/java/com/greenfoxacademy/todowebapp/controllers/TodoRestController.java
@@ -3,22 +3,23 @@ package com.greenfoxacademy.todowebapp.controllers;
 import com.greenfoxacademy.todowebapp.models.Todo;
 import com.greenfoxacademy.todowebapp.services.TodoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TodoRestController {
   @Autowired
   TodoServiceImpl todoService;
 
-  @PostMapping("/todos")
-  public ResponseEntity<List<Todo>> listAllTodos() {
+  @GetMapping("/todos")
+  public ResponseEntity<?> listAllTodos() {
     return ResponseEntity.status(HttpStatus.FOUND).body(todoService.getAllTodos());
+  }
+
+  @PostMapping("/addtodo/{listid}")
+  public ResponseEntity<?> addTodo(@PathVariable(value = "listid") long listid, @RequestBody Todo todo) {
+    todoService.addTodo(listid, todo);
+    return ResponseEntity.status(HttpStatus.CREATED).body(todo);
   }
 }
